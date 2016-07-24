7ca5f063f8c0620bc316e456e9fba7eeae513915
benoitantelme <benoitantelme@gmail.com>
2016-07-24 14:42:51 +0100

    Cleaned and commented file to run tests

--- r/RunTests.R
+++ r/RunTests.R
@@ -1,15 +1,10 @@
 library('RUnit')
 source("r/Utils.R")
 
-
-
+# take the R test files in test directory
 utilsTestSuite <- defineTestSuite("Utils tests",
                                   dirs = "test",
                                   testFileRegexp = '^\\w+\\.R')
 
-
 utilsTestResult <- runTestSuite(utilsTestSuite)
-
-
-
 printTextProtocol(utilsTestResult)
